1ab5e0a607078478295adc200f6722e49881d38d
Your Name <dnyaneshwar.kadam@infeon.in>
2022-10-11 11:31:28 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,22 @@
+const sequelize=require("./util/database");
+const movies=require("./model/movies");
+const movie_year=require("./model/movie_year");
+const Actors=require("./model/Actors");
+const Director=require("./model/Director");
+const Production=require("./model/Production");
+const Writer=require("./model/Writer");
+const Genre=require("./model/Genre");
+const Awords=require("./model/Awords")
+const express=require("express");
+const app= express();
+
+sequelize.sync({alter:true}).then(result=>{
+        console.log(result);
+    }).catch((err)=>{
+        console.log(err);
+
+
+    });
+
+
+ 
---
+++ util/database.js
@@ -0,0 +1,9 @@
+const { Model } = require("sequelize");
+const Sequelize = require("sequelize");
+const sequelize= new Sequelize('idbmDB','root','infeon@1432',
+    {dialect:'mysql',
+    host:'localhost',
+    });
+//console.log("datbase conecton sucessfully")
+
+module.exports=sequelize;
